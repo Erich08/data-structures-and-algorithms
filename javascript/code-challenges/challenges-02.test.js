411bbe11c3f1b7ed66354568c2629d6f2bfcaf17
Erich Hartnauer <erich.hartnauer@gmail.com>
2022-02-28 22:41:52 -0600

    completed challenge 6

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -93,6 +93,8 @@ Write a function named mapTwoToThe that produces the same output as your forLoop
 
 const mapTwoToThe = (arr) => {
   // Solution code here...
+  const newArr = arr.map((num) => 2 ** num);
+  return newArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
