b22968795fec7739998a65e7bfe33ee1fcd08917
Erich Hartnauer <erich.hartnauer@gmail.com>
2022-02-28 22:18:49 -0600

    completed 3rd challenge

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -41,6 +41,9 @@ Write a function named addQuestion that, given an array of strings, uses map to 
 
 const addQuestion = (arr) => {
   // Solution code here...
+  const question = arr.map((questionMark) => questionMark + "?");
+
+  return question;
 };
 
 /* ------------------------------------------------------------------------------------------------
