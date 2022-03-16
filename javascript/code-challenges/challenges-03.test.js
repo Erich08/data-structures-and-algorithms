103a39955178b5384412d890074ba86e09074d41
Erich Hartnauer <erich.hartnauer@gmail.com>
2022-03-16 12:43:02 -0500

    code 3 challenge

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -26,7 +26,8 @@ For example, typeNum([1, 'bob' ,3]) returns [1,3].
 
 const typeNum = (arr) => {
   // Solution code here...
-  
+  const isNum = arr.filter( num => typeof num === 'number');
+  return isNum;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -39,6 +40,8 @@ For example, containsAnd(['panda', 'ran', 'and']) returns ['panda', 'and'].
 
 const containsAnd = (arr) => {
   // Solution code here...
+  const containsAnd = arr.filter(and => and.includes('and'));
+  return containsAnd;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -51,6 +54,8 @@ For example, oddValues([1,2,3]) returns [1,3].
 
 const oddValues = (arr) => {
   // Solution code here...
+  const oddNums = arr.filter(odds => odds %2 === 1);
+  return oddNums;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -63,6 +68,7 @@ For example, notInFirstArray([1,2,3], [1,2,3,4]) returns [4].
 
 const notInFirstArray = (forbiddenValues, arr) => {
   // Solution code here...
+ 
 };
 
 /* ------------------------------------------------------------------------------------------------
